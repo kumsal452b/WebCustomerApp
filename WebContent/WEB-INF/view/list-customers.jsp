914df489f2cb4f707b45d41673011d6b0a036626
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-07 15:24:15 +0300

    web fıle found and fixit

--- WebContent/WEB-INF/view/list-customers.jsp
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>Customer List</title>
 </head>
 <body>
-
+Customer list--coming soon
 </body>
 </html>
--- src/com/kumsal/spring/controller/CustomerController.java
+++ src/com/kumsal/spring/controller/CustomerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+	@RequestMapping("/list")
 	public String listCustomers(Model theModel){
 		
 		return "list-customers";
