dcc3415b6526496a1b21e460ddf7de4f452e8900
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-08 19:43:32 +0300

    nothing

--- src/com/kumsal/spring/controller/CustomerController.java
+++ src/com/kumsal/spring/controller/CustomerController.java
@@ -1,15 +1,28 @@
 package com.kumsal.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kumsal.spring.dao.CustomerDAO;
+import com.kumsal.spring.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel){
 		
+		List<Customer> theCostumer=customerDAO.getCustomer();
+		
+		theModel.addAttribute("customer",theCostumer);
 		return "list-customers";
 	}
 }
