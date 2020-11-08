0ff39b6517050bb98a71176b4369e9def5df7e47
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-08 17:33:52 +0300

    add new annotatıon

--- src/com/kumsal/spring/dao/CustomerDAOImpl.java
+++ src/com/kumsal/spring/dao/CustomerDAOImpl.java
@@ -2,13 +2,17 @@ package com.kumsal.spring.dao;
 
 import java.util.List;
 
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kumsal.spring.entity.Customer;
 
+@Repository
 public class CustomerDAOImpl  implements CustomerDAO{
 
 	@Autowired
@@ -19,9 +23,12 @@ public class CustomerDAOImpl  implements CustomerDAO{
 	public List<Customer> getCustomer() {
 		Session session=sesssinF.getCurrentSession();
 		
+		Query<Customer> theQuery=session.createQuery("from Customer",Customer.class);
+		
+		List<Customer>  rresult=theQuery.getResultList();
 		
 		
-		return null;
+		return rresult;
 	}
 	
 
