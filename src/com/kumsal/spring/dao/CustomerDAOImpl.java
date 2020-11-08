92a797637ceade6326c813a14b0a25b8aba2414e
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-08 17:26:58 +0300

    add sessıon fıelds

--- src/com/kumsal/spring/dao/CustomerDAOImpl.java
+++ src/com/kumsal/spring/dao/CustomerDAOImpl.java
@@ -1,5 +1,28 @@
 package com.kumsal.spring.dao;
 
-public class CustomerDAOImpl {
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kumsal.spring.entity.Customer;
+
+public class CustomerDAOImpl  implements CustomerDAO{
+
+	@Autowired
+	private SessionFactory sesssinF;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomer() {
+		Session session=sesssinF.getCurrentSession();
+		
+		
+		
+		return null;
+	}
+	
 
 }
