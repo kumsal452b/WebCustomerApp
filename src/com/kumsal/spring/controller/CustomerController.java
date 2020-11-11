dde144593d3ef1cd273d7b4f63e72ea41d6fe835
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-11 14:36:36 +0300

    Deleted old lıne ın project

--- src/com/kumsal/spring/controller/CustomerController.java
+++ src/com/kumsal/spring/controller/CustomerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kumsal.spring.dao.CustomerDAO;
@@ -14,15 +16,15 @@ import com.kumsal.spring.entity.Customer;
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	@Autowired
-	private CustomerDAO denek;
 	
-	@RequestMapping("/list")
+	
+	@GetMapping("/list")
 	public String listCustomers(Model theModel){
 		
 		List<Customer> theCostumer=denek.getCustomer();
 		
 		theModel.addAttribute("customer",theCostumer);
+		
 		return "list-customers";
 	}
 }
