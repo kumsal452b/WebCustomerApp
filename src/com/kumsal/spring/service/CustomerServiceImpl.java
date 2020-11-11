4bd83fc6cc4af5ffdc5b4abc38c13c1e4256369a
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-11 15:07:49 +0300

    problem found and solved

--- src/com/kumsal/spring/service/CustomerServiceImpl.java
+++ src/com/kumsal/spring/service/CustomerServiceImpl.java
@@ -3,20 +3,22 @@ package com.kumsal.spring.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kumsal.spring.dao.CustomerDAO;
 import com.kumsal.spring.entity.Customer;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
 	@Autowired
 	private CustomerDAO customerDoa;
 	@Override
 	@Transactional
-	public List<Customer> getCustomers() {
+	public List<Customer> getCustomer() {
 		// TODO Auto-generated method stub
-		return null;
+		return customerDoa.getCustomer();
 	}
 
 }
