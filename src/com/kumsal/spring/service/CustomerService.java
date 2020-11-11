63ed25e4cd709d8c96eca220de13d669ea560ac2
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-11 14:37:40 +0300

    Add new interface

---
+++ src/com/kumsal/spring/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.kumsal.spring.service;
+
+import java.util.List;
+
+import com.kumsal.spring.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
