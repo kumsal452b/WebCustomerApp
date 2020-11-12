60df195a9386073eaac1473c9bd486c020e27199
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-12 13:28:23 +0300

    add new post mappin in systemm

--- src/com/kumsal/spring/controller/CustomerController.java
+++ src/com/kumsal/spring/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,5 +41,10 @@ public class CustomerController {
 		
 		return "customer-form";
 	}
-	public String 
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer){
+		
+		
+		return "redirect:/customer/list";
+	}
 }
