ee3fd2c2d9bc204ca304b9b73dbb00898b850540
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-15 13:11:31 +0300

    modifiyed in the line

---
+++ src/com/kumsal/spring/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.kumsal.spring.service;
+
+import java.util.List;
+
+import com.kumsal.spring.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+	
+}
