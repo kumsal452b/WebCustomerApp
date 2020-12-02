720b4eefdef5cf7cb7e291150c0349be5d33fd3c
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-02 14:36:58 +0300

    addnew pıntcut element

--- src/com/kumsal/spring/aspect/CRMLoggingAspect.java
+++ src/com/kumsal/spring/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.kumsal.spring.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,4 +11,24 @@ import org.springframework.stereotype.Component;
 public class CRMLoggingAspect {
 	private Logger myLogger=Logger.getLogger(getClass().getName());
 	
+	@Pointcut("execution(* com.kumsal.spring.controller.*.*.(..))")
+	private void forControllPackacge(){
+		
+	}
+	
+		
+	@Pointcut("execution(* com.kumsal.spring.service.*.*.(..))")
+	private void forServicePackacge(){
+		
+	}
+	
+	@Pointcut("execution(* com.kumsal.spring.dao.*.*.(..))")
+	private void forDaoPackacge(){
+		
+	}
+	
+	@Pointcut("forControllPackacge || forServicePackacge || forDaoPackacge")
+	private void forAppFlow(){
+		
+	}
 }
