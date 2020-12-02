36ea3f402d3da92dfdfdf358b89184334cbdf04a
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-02 14:43:02 +0300

    fix it

--- src/com/kumsal/spring/aspect/CRMLoggingAspect.java
+++ src/com/kumsal/spring/aspect/CRMLoggingAspect.java
@@ -2,7 +2,9 @@ package com.kumsal.spring.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -11,24 +13,33 @@ import org.springframework.stereotype.Component;
 public class CRMLoggingAspect {
 	private Logger myLogger=Logger.getLogger(getClass().getName());
 	
-	@Pointcut("execution(* com.kumsal.spring.controller.*.*.(..))")
+	@Pointcut("execution(* com.kumsal.spring.controller.*.*(..))")
 	private void forControllPackacge(){
 		
 	}
 	
 		
-	@Pointcut("execution(* com.kumsal.spring.service.*.*.(..))")
+	@Pointcut("execution(* com.kumsal.spring.service.*.*(..))")
 	private void forServicePackacge(){
 		
 	}
 	
-	@Pointcut("execution(* com.kumsal.spring.dao.*.*.(..))")
+	@Pointcut("execution(* com.kumsal.spring.dao.*.*(..))")
 	private void forDaoPackacge(){
 		
 	}
 	
 	@Pointcut("forControllPackacge || forServicePackacge || forDaoPackacge")
 	private void forAppFlow(){
+		
+	}
+	
+	@Before("forAppFlow")
+	public void before(JoinPoint thePoint){
+		String method=thePoint.getSignature().toString();
+		myLogger.info("Before calling Method====>"+method);
+		
+		
 		
 	}
 }
